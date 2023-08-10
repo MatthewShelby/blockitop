4e8c03d66050225340f74b0bacd7a8f0744612a9
MatthewShelby <matthewshelb@gmail.com>
2023-08-10 17:48:41 +0330

    just imp 2

--- assets/js/header-footer.js
+++ assets/js/header-footer.js
@@ -1,4 +1,4 @@
-addHeaderFooter()
+//addHeaderFooter()
 function addHeaderFooter2(imp) {
       console.log('imp: ' + imp)
       fetch(imp + "header.txt" /*, options */)
@@ -23,26 +23,26 @@ function addHeaderFooter2(imp) {
                   console.warn(error);
             });
 }
-function addHeaderFooter() {
-      fetch("../../header.txt" /*, options */)
-            .then((response) => response.text())
-            .then((html) => {
-                  $('header')[0].innerHTML = html;
-                  $('header')[0].classList.add('sticky-top')
-                  document.getElementById('network-name').innerHTML = 'BSC';
-                  document.getElementById('network-name').innerHTML = 'BSC';
+// function addHeaderFooter() {
+//       fetch("../../header.txt" /*, options */)
+//             .then((response) => response.text())
+//             .then((html) => {
+//                   $('header')[0].innerHTML = html;
+//                   $('header')[0].classList.add('sticky-top')
+//                   document.getElementById('network-name').innerHTML = 'BSC';
+//                   document.getElementById('network-name').innerHTML = 'BSC';
 
-            })
-            .catch((error) => {
-                  console.warn(error);
-            });
-      fetch("../../footer.txt" /*, options */)
-            .then((response) => response.text())
-            .then((html) => {
-                  $('footer')[0].innerHTML = html;
-                  //$('header')[0].classList.add('sticky-top')
-            })
-            .catch((error) => {
-                  console.warn(error);
-            });
-}
+//             })
+//             .catch((error) => {
+//                   console.warn(error);
+//             });
+//       fetch("../../footer.txt" /*, options */)
+//             .then((response) => response.text())
+//             .then((html) => {
+//                   $('footer')[0].innerHTML = html;
+//                   //$('header')[0].classList.add('sticky-top')
+//             })
+//             .catch((error) => {
+//                   console.warn(error);
+//             });
+// }
