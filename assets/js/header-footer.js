a0146351de4bd6aa85ff8326bb58330f9d27feae
MatthewShelby <matthewshelb@gmail.com>
2023-08-10 17:54:59 +0330

    heder footer address back to html

--- assets/js/header-footer.js
+++ assets/js/header-footer.js
@@ -1,7 +1,7 @@
-//addHeaderFooter()
-function addHeaderFooter2(imp) {
-      console.log('imp: ' + imp)
-      fetch(imp + "header.txt" /*, options */)
+addHeaderFooter()
+
+function addHeaderFooter() {
+      fetch("./header.html" /*, options */)
             .then((response) => response.text())
             .then((html) => {
                   $('header')[0].innerHTML = html;
@@ -13,7 +13,7 @@ function addHeaderFooter2(imp) {
             .catch((error) => {
                   console.warn(error);
             });
-      fetch(imp + "footer.txt" /*, options */)
+      fetch("./footer.html" /*, options */)
             .then((response) => response.text())
             .then((html) => {
                   $('footer')[0].innerHTML = html;
@@ -23,26 +23,3 @@ function addHeaderFooter2(imp) {
                   console.warn(error);
             });
 }
-// function addHeaderFooter() {
-//       fetch("../../header.txt" /*, options */)
-//             .then((response) => response.text())
-//             .then((html) => {
-//                   $('header')[0].innerHTML = html;
-//                   $('header')[0].classList.add('sticky-top')
-//                   document.getElementById('network-name').innerHTML = 'BSC';
-//                   document.getElementById('network-name').innerHTML = 'BSC';
-
-//             })
-//             .catch((error) => {
-//                   console.warn(error);
-//             });
-//       fetch("../../footer.txt" /*, options */)
-//             .then((response) => response.text())
-//             .then((html) => {
-//                   $('footer')[0].innerHTML = html;
-//                   //$('header')[0].classList.add('sticky-top')
-//             })
-//             .catch((error) => {
-//                   console.warn(error);
-//             });
-// }
