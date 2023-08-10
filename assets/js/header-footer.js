631437c5e25660755d8c83ca870f907feb79b913
MatthewShelby <matthewshelb@gmail.com>
2023-08-10 17:41:50 +0330

    imp for header

--- assets/js/header-footer.js
+++ assets/js/header-footer.js
@@ -1,4 +1,27 @@
 addHeaderFooter()
+function addHeaderFooter(imp) {
+      fetch(imp + "header.txt" /*, options */)
+            .then((response) => response.text())
+            .then((html) => {
+                  $('header')[0].innerHTML = html;
+                  $('header')[0].classList.add('sticky-top')
+                  document.getElementById('network-name').innerHTML = 'BSC';
+                  document.getElementById('network-name').innerHTML = 'BSC';
+
+            })
+            .catch((error) => {
+                  console.warn(error);
+            });
+      fetch(imp + "footer.txt" /*, options */)
+            .then((response) => response.text())
+            .then((html) => {
+                  $('footer')[0].innerHTML = html;
+                  //$('header')[0].classList.add('sticky-top')
+            })
+            .catch((error) => {
+                  console.warn(error);
+            });
+}
 function addHeaderFooter() {
       fetch("../../header.txt" /*, options */)
             .then((response) => response.text())
